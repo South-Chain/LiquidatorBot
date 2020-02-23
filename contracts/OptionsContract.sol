763b73c5ab1662744efb0aa2550a2b196463da06
Anton Cheng <anton@coolbitx.com>
2020-02-22 23:31:36 -0800

    Add optionsExchange method

--- contracts/OptionsContract.sol
+++ contracts/OptionsContract.sol
@@ -1,8 +1,18 @@
 pragma solidity 0.5.10;
 
+
 contract OptionsContract {
-        function liquidate(address payable vaultOwner, uint256 oTokensToLiquidate) public;
-        function  maxOTokensLiquidatable(address payable vaultOwner) public view returns (uint256);
-        function isUnsafe(address payable vaultOwner) public view returns (bool);
-        function approve(address spender, uint256 amount) public returns (bool);
+    function liquidate(address payable vaultOwner, uint256 oTokensToLiquidate)
+        public;
+
+    function optionsExchange() public returns (address);
+
+    function maxOTokensLiquidatable(address payable vaultOwner)
+        public
+        view
+        returns (uint256);
+
+    function isUnsafe(address payable vaultOwner) public view returns (bool);
+
+    function approve(address spender, uint256 amount) public returns (bool);
 }

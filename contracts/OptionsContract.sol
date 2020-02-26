80da33e11cb6c743ed1dc2cba39c100fdfe8966c
Anton Cheng <anton@coolbitx.com>
2020-02-26 11:18:17 +0800

    Add methods to optionscontract interface

--- contracts/OptionsContract.sol
+++ contracts/OptionsContract.sol
@@ -14,5 +14,26 @@ contract OptionsContract {
 
     function isUnsafe(address payable vaultOwner) public view returns (bool);
 
+    function hasVault(address valtowner) public view returns (bool);
+
+    function openVault() public returns (bool);
+
+    function addETHCollateral(address payable vaultOwner)
+        public
+        payable
+        returns (uint256);
+
+    function maxOTokensIssuable(uint256 collateralAmt)
+        public
+        view
+        returns (uint256);
+
+    function getVault(address payable vaultOwner)
+        public
+        view
+        returns (uint256, uint256, uint256, bool);
+
+    function issueOTokens(uint256 oTokensToIssue, address receiver) public;
+
     function approve(address spender, uint256 amount) public returns (bool);
 }

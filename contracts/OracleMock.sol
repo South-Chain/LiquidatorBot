a0d4093cbed591cf2c09bd3b44cd592957f18acc
Anton Cheng <anton@coolbitx.com>
2020-03-08 22:51:01 +0800

    Update mock oracle

--- contracts/OracleMock.sol
+++ contracts/OracleMock.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.5.0;
 interface Oracle {
     function getPrice(address _token) external pure returns (uint256);
 
-    function updatePrice(uint256 _newPrice) external;
+    function updatePrice(address _token, uint256 _newPrice) external;
 }

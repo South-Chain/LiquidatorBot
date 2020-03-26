6bf14b1c7e0b2ffc99b40b85b384c4dca8900ace
Anton Cheng <anton@coolbitx.com>
2020-03-26 20:58:14 +0800

    Convert to ERC20 Liquidator

--- contracts/Liquidator.sol
+++ contracts/Liquidator.sol
@@ -1,19 +1,25 @@
 pragma solidity 0.5.10;
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./FlashLoanReceiverBase.sol";
 import "./OptionsContract.sol";
 import "./OptionsExchange.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./ILendingPoolAddressesProvider.sol";
+import "./IUniswapFactory.sol";
+import "./IUniswapExchange.sol";
 
 
 contract Liquidator is FlashLoanReceiverBase {
     using SafeMath for uint256;
+    IUniswapFactory public factory;
 
-    constructor(ILendingPoolAddressesProvider ILendingAddress)
-        public
-        FlashLoanReceiverBase(ILendingAddress)
-    {}
+    constructor(
+        ILendingPoolAddressesProvider _provider,
+        IUniswapFactory _factory
+    ) public FlashLoanReceiverBase(_provider) {
+        factory = _factory;
+    }
 
     function executeOperation(
         address _reserve,
@@ -21,27 +27,44 @@ contract Liquidator is FlashLoanReceiverBase {
         uint256 _fee,
         bytes calldata _params
     ) external {
+        // 1. Parse params
         (address oTokenAddr, address vaultAddr) = getParams(_params);
         address payable vaultOwner = address(uint160(vaultAddr));
         OptionsContract oToken = OptionsContract(oTokenAddr);
-        // 1. Get _amount from the reserve pool
+
         // 2. Buy oTokens on uniswap
         uint256 oTokensToBuy = oToken.maxOTokensLiquidatable(vaultOwner);
         require(oTokensToBuy > 0, "cannot liquidate a safe vault");
 
         OptionsExchange exchange = OptionsExchange(oToken.optionsExchange());
-        exchange.buyOTokens.value(_amount)(
+
+        // address paymentToken = _reserve;
+        IERC20(_reserve).approve(address(exchange), _amount);
+        exchange.buyOTokens(
             address(uint160(address(this))),
             oTokenAddr,
-            address(0),
+            _reserve,
             oTokensToBuy
         );
         // 3. Liquidate
         oToken.liquidate(vaultOwner, oTokensToBuy);
-        // 4. pay back the $
+        // 4. Use eth to buy back _reserve
+        IUniswapExchange uniswap = IUniswapExchange(
+            factory.getExchange(_reserve)
+        );
+        uint256 buyAmount = uniswap.getEthToTokenInputPrice(
+            address(this).balance
+        );
+        uniswap.ethToTokenSwapInput.value(address(this).balance)(
+            buyAmount,
+            now + 10 minutes
+        );
+
+        // // 5. pay back the $
         transferFundsBackToPoolInternal(_reserve, _amount.add(_fee));
-        // 5. pay the user who liquidated
-        tx.origin.transfer(address(this).balance);
+        // // 6. pay the user who liquidated
+        uint256 balance = IERC20(_reserve).balanceOf(address(this));
+        IERC20(_reserve).transfer(tx.origin, balance);
     }
 
     function bytesToAddress(bytes memory bys)
